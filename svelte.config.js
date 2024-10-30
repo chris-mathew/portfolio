bce1c3589188e594f7754b99440573fed8de419e
cmm223icacuk <cmm223@ic.ac.uk>
2024-10-31 01:59:00 +0400

    - Publishing to build folder instead of output

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-netlify';
 
 export default {
-  // Configure the preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // Add the Netlify adapter here
-    adapter: adapter(),
-    // Other kit options can be added here as needed
+    adapter: adapter({
+      // Ensure the output directory matches what Netlify expects
+      pages: 'build',
+      assets: 'build'
+    })
   }
 };

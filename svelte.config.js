5da39f34709060dbc76cdb652bc8bb4f928100fb
cmm223icacuk <cmm223@ic.ac.uk>
2024-10-31 01:53:29 +0400

    - Added Netlify configurations

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-netlify';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+  // Configure the preprocessors
   preprocess: vitePreprocess(),
-}
+
+  kit: {
+    // Add the Netlify adapter here
+    adapter: adapter(),
+    // Other kit options can be added here as needed
+  }
+};
